1a32e6a81893bb996056476c817235810ad6fa5e
helio <heliojmsemedo@gmail.com>
2019-10-05 23:36:56 +0100

    Updates

--- precache-manifest.63c631fe1d22ebc5ffc70552cf1d505d.js
+++ precache-manifest.63c631fe1d22ebc5ffc70552cf1d505d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ede01496246b1ced16b7e150fc824bbb",
+    "revision": "d5d001f6f272e3d30a202b62b577c792",
     "url": "/big_skroll/index.html"
   },
   {
-    "revision": "093b191eb4cb5b38777f",
+    "revision": "5b60765a4d014019cbc2",
     "url": "/big_skroll/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "5446881baa5664a04630",
-    "url": "/big_skroll/static/js/2.61b59dde.chunk.js"
+    "revision": "b6df5173b672ed2996be",
+    "url": "/big_skroll/static/js/2.08b60fab.chunk.js"
   },
   {
-    "revision": "093b191eb4cb5b38777f",
-    "url": "/big_skroll/static/js/main.181e286b.chunk.js"
+    "revision": "5b60765a4d014019cbc2",
+    "url": "/big_skroll/static/js/main.97dd23c5.chunk.js"
   },
   {
     "revision": "71446415fffc0b83431f",
